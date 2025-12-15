90182683589f9e1f3114bca3908cf65105714efd
Sha Fin <99030043+shshafin@users.noreply.github.com>
2025-12-16 02:06:49 +0600

    changes

--- .history/src/app_20251216020633.ts
+++ .history/src/app_20251216020633.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://gsl-client.vercel.app'],
+    origin: ['http://localhost:3000',''],
     credentials: true,
   }),
 );
